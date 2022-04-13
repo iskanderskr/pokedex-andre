0d3c08f47aadc24aca3c435dfab73f08f69bcabb
cauandre <carvalho-andre2011@hotmail.com>
2022-04-13 16:49:45 -0300

    Pokedex with Resful using Express in Node.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const Pokedex = require('pokedex')
 const app = express();
 const port = 3000;
+const pokedex = new Pokedex();
+ 
+
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Bem Vindo a Pokedex");
 });
 
-app.get("/pokemon", function (req, res) {
-  res.send("Pokemon");
+app.get("/pokemoninicial", function (req, res) {
+  res.send(pokedex.pokemon('squirtle'));
 });
-
-app.get("/teste", function (req, res) {
-  res.send("Teste");
+app.get("/todospokemon", function(req, res) {
+  res.send(pokedex.pokemon(''));
 });
 
 app.listen(port, () => {
